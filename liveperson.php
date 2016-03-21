3222d5c8f7c72b053593e6c2d1f915e964ba1438
rszrama <ryan.szrama@gmail.com>
2016-03-21 17:33:11 -0400

    Add the stub for the WordPress plugin file.

---
+++ liveperson.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: LivePerson LiveChat + Messaging
+ * Plugin URI:  https://register.liveperson.com/wordpress
+ * Description: LivePerson offers a modern way to talk to visitors regardless of how they visit your website. This plugin adds it to specified pages on your site.
+ * Version:     1.0.0
+ * Author:      LivePerson, Inc.
+ * Author URI:  http://www.liveperson.com
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// Exit if this file was accessed directly.
+if (!defined('ABSPATH')) {
+  exit;
+}

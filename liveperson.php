55dd7ce7d0869391764eb98d3303fc43410a6244
rszrama <ryan.szrama@gmail.com>
2016-03-24 18:24:07 -0400

    Add a settings menu item and form for the plugin.

--- liveperson.php
+++ liveperson.php
@@ -14,3 +14,51 @@
 if (!defined('ABSPATH')) {
   exit;
 }
+
+// Register this plugin's hooks.
+add_action('admin_menu', 'liveperson_admin_menu');
+add_action('admin_init', 'liveperson_admin_init');
+
+/**
+ * Hooks into admin_menu to add a LivePerson options page to the Settings menu.
+ */
+function liveperson_admin_menu() {
+  add_options_page('LivePerson', 'LivePerson', 'manage_options', 'liveperson', 'liveperson_options_page');
+}
+
+/**
+ * Hooks into admin_init to register the LivePerson settings.
+ */
+function liveperson_admin_init() {
+  register_setting('pluginPage', 'liveperson_settings');
+  add_settings_section('liveperson_pluginPage_settings', __('Section title', 'liveperson'), 'liveperson_settings_section_callback', 'pluginPage');
+  add_settings_field('liveperson_account_number', __('Account number', 'liveperson'), 'liveperson_account_number_render', 'pluginPage', 'liveperson_pluginPage_settings');
+}
+
+/**
+ * Renders the account number form field.
+ */
+function liveperson_account_number_render() {
+  $options = get_option('liveperson_settings');
+  echo '<input type="text" name="liveperson_settings[liveperson_account_number]" value="' . $options['liveperson_account_number'] . '"">';
+}
+
+/**
+ * Renders the settings section description.
+ */
+function liveperson_settings_section_callback() {
+  echo __('This is a section description', 'liveperson');
+}
+
+/**
+ * Outputs the LivePerson options page content.
+ */
+function liveperson_options_page() {
+  echo '<form action="options.php" method="post"><h2>LivePerson</h2>';
+
+  settings_fields('pluginPage');
+  do_settings_sections('pluginPage');
+  submit_button();
+
+  echo '</form>';
+}

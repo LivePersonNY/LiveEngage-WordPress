52f9783672daec476782ca6b90d2f5a82c5bf2b5
rszrama <ryan.szrama@gmail.com>
2016-03-28 16:12:30 -0400

    Add the JavaScript tag on the footer of all front-end pages via the wp_footer hook.

--- liveperson.php
+++ liveperson.php
@@ -18,6 +18,7 @@ if (!defined('ABSPATH')) {
 // Register this plugin's hooks.
 add_action('admin_menu', 'liveperson_admin_menu');
 add_action('admin_init', 'liveperson_admin_init');
+add_action('wp_footer', 'liveperson_wp_footer');
 
 // Register this plugin's filters.
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'liveperson_settings_link');
@@ -40,19 +41,22 @@ function liveperson_settings_link($links) {
 
 /**
  * Hooks into admin_init to register the LivePerson settings.
+ *
+ * @todo Add checkboxes to filter tag visibility by user role.
+ * @todo Add checkboxes to filter tag visibility by post type.
+ * @todo Add textarea to filter tag visibility by path.
  */
 function liveperson_admin_init() {
   register_setting('pluginPage', 'liveperson_settings');
   add_settings_section('liveperson_pluginPage_settings', __('Section title', 'liveperson'), 'liveperson_settings_section_callback', 'pluginPage');
-  add_settings_field('liveperson_account_number', __('Account number', 'liveperson'), 'liveperson_account_number_render', 'pluginPage', 'liveperson_pluginPage_settings');
+  add_settings_field('liveperson_account_number', __('Account number', 'liveperson'), 'liveperson_account_number_field_render', 'pluginPage', 'liveperson_pluginPage_settings');
 }
 
 /**
  * Renders the account number form field.
  */
-function liveperson_account_number_render() {
-  $options = get_option('liveperson_settings');
-  echo '<input type="text" name="liveperson_settings[liveperson_account_number]" value="' . $options['liveperson_account_number'] . '"">';
+function liveperson_account_number_field_render() {
+  echo '<input type="text" name="liveperson_settings[liveperson_account_number]" value="' . liveperson_account_number() . '"">';
 }
 
 /**
@@ -73,4 +77,26 @@ function liveperson_options_page() {
   submit_button();
 
   echo '</form>';
+}
+
+/**
+ * Returns the site's LivePerson account number.
+ */
+function liveperson_account_number() {
+  $options = get_option('liveperson_settings');
+  return $options['liveperson_account_number'];
+}
+
+/**
+ * Returns the LivePerson JavaScript tag for the given accoutn number.
+ */
+function liveperson_javascript_tag($account_number) {
+  return "<script>window.lpTag=window.lpTag||{};if(typeof window.lpTag._tagCount==='undefined'){window.lpTag={site:'" . $account_number . "'||'',section:lpTag.section||'',autoStart:lpTag.autoStart===false?false:true,ovr:lpTag.ovr||{},_v:'1.5.1',_tagCount:1,protocol:location.protocol,events:{bind:function(app,ev,fn){lpTag.defer(function(){lpTag.events.bind(app,ev,fn);},0);},trigger:function(app,ev,json){lpTag.defer(function(){lpTag.events.trigger(app,ev,json);},1);}},defer:function(fn,fnType){if(fnType==0){this._defB=this._defB||[];this._defB.push(fn);}else if(fnType==1){this._defT=this._defT||[];this._defT.push(fn);}else{this._defL=this._defL||[];this._defL.push(fn);}},load:function(src,chr,id){var t=this;setTimeout(function(){t._load(src,chr,id);},0);},_load:function(src,chr,id){var url=src;if(!src){url=this.protocol+'//'+((this.ovr&&this.ovr.domain)?this.ovr.domain:'lptag.liveperson.net')+'/tag/tag.js?site='+this.site;}var s=document.createElement('script');s.setAttribute('charset',chr?chr:'UTF-8');if(id){s.setAttribute('id',id);}s.setAttribute('src',url);document.getElementsByTagName('head').item(0).appendChild(s);},init:function(){this._timing=this._timing||{};this._timing.start=(new Date()).getTime();var that=this;if(window.attachEvent){window.attachEvent('onload',function(){that._domReady('domReady');});}else{window.addEventListener('DOMContentLoaded',function(){that._domReady('contReady');},false);window.addEventListener('load',function(){that._domReady('domReady');},false);}if(typeof(window._lptStop)=='undefined'){this.load();}},start:function(){this.autoStart=true;},_domReady:function(n){if(!this.isDom){this.isDom=true;this.events.trigger('LPT','DOM_READY',{t:n});}this._timing[n]=(new Date()).getTime();},vars:lpTag.vars||[],dbs:lpTag.dbs||[],ctn:lpTag.ctn||[],sdes:lpTag.sdes||[],ev:lpTag.ev||[]};lpTag.init();}else{window.lpTag._tagCount+=1;}</script>";
+}
+
+/**
+ * Hooks into wp_footer to add the JavaScript tag on appropriate pages.
+ */
+function liveperson_wp_footer() {
+  echo liveperson_javascript_tag(liveperson_account_number());
 }
